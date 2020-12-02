cc9f5224cccd8f0b87e7446a76b25a5635f66e33
Mohd. Asfaq-E-Azam Rifat <mohd.asfaqeazam@gmail.com>
2020-12-02 19:56:14 +0600

    add view binding and click listener

--- app/src/main/java/com/tokbox/test/diceroller/MainActivity.kt
+++ app/src/main/java/com/tokbox/test/diceroller/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.tokbox.test.diceroller
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.tokbox.test.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(this.layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.buttonRoll.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
